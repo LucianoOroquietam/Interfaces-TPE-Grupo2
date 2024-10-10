1319a62f6e5e8f36a3bc69f3306baa96c7079902
Luciano <oroquietaluciano@gmail.com>
2024-10-10 12:43:45 -0300

    carrusel fixeado

--- TP2/js/carrousel.js
+++ TP2/js/carrousel.js
@@ -2,40 +2,39 @@
 const carruseles = document.querySelectorAll(".carrusel-wrapper");
 
 carruseles.forEach((carrusel) => {
-    // Obtenemos los botones y el contenedor del carrusel específico
-    const rightBtn = carrusel.querySelector(".scrolling-buttons-container span:nth-child(1)"); // Botón derecho
-    const leftBtn = carrusel.querySelector(".scrolling-buttons-container span:nth-child(2)"); // Botón izquierdo
-    const content = carrusel.nextElementSibling; // El contenedor de las tarjetas
-    const cardWidth = content.querySelector(".scrolling-card").offsetWidth; // Ancho de una tarjeta
+    const rightBtn = carrusel.querySelector(".scrolling-buttons-container span:nth-child(1)");
+    const leftBtn = carrusel.querySelector(".scrolling-buttons-container span:nth-child(2)");
+    const content = carrusel.nextElementSibling;
+    const cardWidth = content.querySelector(".scrolling-card").offsetWidth;
 
-    // Función para desplazarse a la derecha
+
     const scrollRight = () => {
         const maxScrollLeft = content.scrollWidth - content.clientWidth; // Máximo desplazamiento horizontal
         if (content.scrollLeft + cardWidth >= maxScrollLeft) {
-            content.scrollLeft = maxScrollLeft; // Si llega al final, lo detenemos al final
+            content.scrollLeft = maxScrollLeft;
         } else {
             content.classList.add("skew1");
             setTimeout(() => {
                 content.classList.remove("skew1");
             }, 350);
-            content.scrollLeft += cardWidth; // Desplaza una tarjeta a la derecha
+            content.scrollLeft += cardWidth;
         }
     };
 
-    // Función para desplazarse a la izquierda
+
     const scrollLeft = () => {
         if (content.scrollLeft - cardWidth <= 0) {
-            content.scrollLeft = 0; // Si llega al principio, lo detenemos al inicio
+            content.scrollLeft = 0;
         } else {
             content.classList.add("skew1");
             setTimeout(() => {
                 content.classList.remove("skew1");
             }, 350);
-            content.scrollLeft -= cardWidth; // Desplaza una tarjeta a la izquierda
+            content.scrollLeft -= cardWidth;
         }
     };
 
-    // Eventos de clic para los botones
+
     rightBtn.addEventListener("click", scrollRight);
     leftBtn.addEventListener("click", scrollLeft);
 });
