9be2f210b1dab78cd9c84458600a709ec02ac69a
Luciano <oroquietaluciano@gmail.com>
2024-10-14 12:17:29 -0300

    se corrigen tamaños en media-query , se agrega comportamiento a los carritos del carrusel

---
+++ TP2/js/carrito-iteraccion.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("El archivo JS se ha cargado correctamente");
+    
+    const imgs = document.querySelectorAll('.carrito-compra');
+
+        console.log("Imágenes encontradas correctamente en el DOM");
+        
+        imgs.forEach(function(img) {  
+            img.addEventListener('click', function() {
+                if (img.src.includes('Agregar.png')) {
+                    img.src = 'img/detalle card/Add Shopping Cart.png';
+                } else {
+                    img.src = 'img/detalle card/Agregar.png';
+                }
+            });
+        });
+    
+});
