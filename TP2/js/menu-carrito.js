3488917f18c8a9befca95783369da3c7a8ab94c1
Luciano <oroquietaluciano@gmail.com>
2024-10-14 22:40:05 -0300

    se fixea menu

--- TP2/js/menu-carrito.js
+++ TP2/js/menu-carrito.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const menuPerfil = document.getElementById('perfil');
 const navPerfil = document.getElementById('nav-perfil');
+const body = document.querySelector('.background-menu')
 
 // Evento click sobre el icono menu hamburguesa
 menuPerfil.addEventListener('click', () => {
-    
     navPerfil.classList.toggle('nav-perfil--active'); 
     body.classList.toggle('transparent-background');
 });
@@ -20,7 +20,6 @@ const navCarrito = document.querySelector('.cart-container');
 
 
 menuCarrito.addEventListener('click', () => {
-    console.log("estoy haciendo click")
     navCarrito.classList.toggle('nav-carrito--active'); 
     body.classList.toggle('transparent-background');
 })
